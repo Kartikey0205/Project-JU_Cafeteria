b62e0aac1d6b809e101d02cd1b2c9469218aec8c
Kartikey Dubey <dkartikey1@gmail.com>
2022-04-27 22:59:30 +0530

    fetch home page menu from database and make connection with database

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
+require("dotenv").config();
+
 // Express Server required package
 const express = require("express");
-const app = express();
 const ejs = require("ejs");
 const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
 
+// Database stuff
+const mongoose = require("mongoose");
+
+// requiring all links that coming from env file via setup directory and my file inside it
+const myfile = require("./setup/myFile");
+
+const app = express();
 // Dynamic Port
 const PORT = process.env.PORT || 5500;
+
+// Database connection
+const db = myfile.dbUrl;
+//Attempt to connect to database
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("MongoDB connected succesfully..");
+  })
+  .catch((err) => {
+    console.log("Error is ", err);
+  });
 
 // Assests for MIME type accrordingly setting Content Type
 app.use(express.static("public"));
