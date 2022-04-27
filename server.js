b60e9ca61ebe15aabd77c022da8459944bb54b6f
Kartikey Dubey <dkartikey1@gmail.com>
2022-04-27 23:19:53 +0530

    created session and cookie

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const path = require("path");
 
 // Database stuff
 const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("express-flash");
+
+// connect-mongo is used to store session in database
+const MongoDbStore = require("connect-mongo");
 
 // requiring all links that coming from env file via setup directory and my file inside it
 const myfile = require("./setup/myFile");
@@ -23,8 +28,6 @@ mongoose
   .connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false,
   })
   .then(() => {
     console.log("MongoDB connected succesfully..");
@@ -32,6 +35,28 @@ mongoose
   .catch((err) => {
     console.log("Error is ", err);
   });
+
+// Session Config
+// for creating session always do the same
+//  Store is used for storing the session in database
+app.use(
+  session({
+    secret: myfile.secret,
+    resave: false,
+    store: MongoDbStore.create({
+      mongoUrl: db,
+    }),
+    saveUninitialized: true,
+    cookie: {
+      //  secure: true
+      maxAge: 1000 * 60 * 60 * 24,
+      // maxAge: 1000 * 50,
+    },
+  })
+);
+
+// Use flash Middleware
+app.use(flash());
 
 // Assests for MIME type accrordingly setting Content Type
 app.use(express.static("public"));
