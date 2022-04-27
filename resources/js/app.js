c14a05e05b5d682f322924d9de76d6bc939c77c9
Kartikey Dubey <dkartikey1@gmail.com>
2022-04-27 16:48:42 +0530

    Completed Home Page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-console.log("Heelo");
+// Swipper Js cODE
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
