17bd146e6faaa5d7da616782ce51e0b307372573
Kartikey Dubey <dkartikey1@gmail.com>
2022-04-27 17:02:58 +0530

    Made layout.ejs for separting common code from side ejs layout

--- server.js
+++ server.js
@@ -11,17 +11,19 @@ const PORT = process.env.PORT || 5500;
 // Assests for MIME type accrordingly setting Content Type
 app.use(express.static("public"));
 
+// set Template Engine
+app.use(expressLayouts);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
+
 // routes
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-// set Template Engine
-
-app.use(expressLayouts);
-app.set("views", path.join(__dirname, "/resources/views"));
-app.set("view engine", "ejs");
-
+app.get("/cart", (req, res) => {
+  res.render("customer/cart");
+});
 // Listening to server
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
