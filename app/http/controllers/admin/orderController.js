7e2e8b6bf485af4d90d0b6f6afdf9548ee8b1b21
Kartikey Dubey <dkartikey1@gmail.com>
2022-05-30 19:29:10 +0530

    implemented admin logic

---
+++ app/http/controllers/admin/orderController.js
@@ -0,0 +1,22 @@
+const Order = require("../../../models/Order");
+
+function orderController() {
+  return {
+    index(req, res) {
+      Order.find({ status: { $ne: "completed" } }, null, {
+        sort: { createdAt: -1 },
+      })
+        .populate("customerId", "-password")
+        .exec((err, orders) => {
+          // Ajax call using axios nut not direct call so sending data in json form
+          if (req.xhr) {
+            // console.log(req);
+            return res.json(orders);
+          } else {
+            return res.render("admin/orders");
+          }
+        });
+    },
+  };
+}
+module.exports = orderController;
